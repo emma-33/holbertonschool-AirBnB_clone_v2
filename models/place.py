f7471bf8e936f14c6c4f1fc9e55c02852ac44163
emma-33 <emma.lateyron@orange.fr>
2024-03-29 09:18:24 +0100

    [add] amenity.py place.py review.py state.py

--- models/place.py
+++ models/place.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
-from sqlalchemy import Column, String, ForeignKey, Integer, Float
+from models.amenity import Amenity
+import models
+from models.review import Review
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
@@ -18,3 +23,42 @@ class Place(BaseModel):
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     amenity_ids = []
+
+    metadata = Base.metadata
+    
+    place_amenity = Table('place_amenity', metadata,
+                            Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
+                            Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False)
+                            )
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        reviews = relationship('Review', backref='place', cascade='delete')
+        amenities = relationship('Amenity', secondary='place_amenity',
+                                     viewonly=False, overlaps="place_amenities")
+
+    else:
+        @property
+        def reviews(self):
+            """Returns the list of Review instances"""
+            reviews_instances = []
+
+            for review in models.storage.all(Review).values():
+                if review.place_id == self.id:
+                    reviews_instances.append(review)
+            return reviews_instances
+        
+        @property
+        def amenities(self):
+            """Returns the list of Amenities instances"""
+            amenities_instances = []
+
+            for amenity in models.storage.all(Amenity).values():
+                if amenity.id in self.amenity_ids:
+                    amenities_instances.append(amenity)
+            return amenities_instances
+        
+        @amenities.setter
+        def amenities(self, obj):
+            """Amenities setter method"""
+            if type(obj) == Amenity():
+                self.amenity_ids.append(obj)
--- models/state.py
+++ models/state.py
@@ -3,7 +3,7 @@
 import models
 from models.base_model import BaseModel, Base
 from models.city import City
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from os import getenv
 
@@ -12,7 +12,7 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities_rel = relationship("City", backref="state", cascade="all, delete")
+    cities = relationship('City', backref='state', cascade='delete')
 
     if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
